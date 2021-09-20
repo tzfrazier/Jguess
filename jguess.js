0b999a27cec3c7f5feaa1b1f0a13ddda99209baa
Zack <t.zachary.frazier@gmail.com>
2021-09-20 14:32:49 -0400

    Added some flair stuff for class to build from.

--- jguess.js
+++ jguess.js
@@ -1,5 +1,15 @@
 // Generate and store a random int
+let RandomNum //code for generating a random number
 
+//code to take in input
+var Guess //
 // check guess against that number
 
-// display results
+
+  if (RandomNum == Guess){
+    //display winning message
+  } else if (RandomNum < Guess){
+    //display too high msg
+  } else if (RandomNum > Guess)  {
+    //display too low msg
+  }
